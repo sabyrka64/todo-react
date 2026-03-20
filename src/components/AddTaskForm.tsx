7088c2d89f2268fc51497897b3c123e46a2d19fb
Sabyrka <sabyrka64@gmail.com>
2026-03-20 16:14:03 +0500

    feat: make Field component reusable with props

--- src/components/AddTaskForm.tsx
+++ src/components/AddTaskForm.tsx
@@ -4,7 +4,11 @@ import { Button } from './Button'
 export const AddTaskForm = () => {
   return (
     <form className="todo__form">
-      <Field />
+      <Field
+        className="todo__field"
+        label="New task title"
+        id="new-task"
+      />
       <Button />
     </form>
   )
--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,17 +1,27 @@
-export const Field = () => {
+import clsx from 'clsx'
+import { type InputHTMLAttributes } from 'react'
+
+type FieldProps = {
+  className?: string
+  id: string
+  label: string
+} & InputHTMLAttributes<HTMLInputElement>
+
+export const Field = ({ className = '', id, label, ...props }: FieldProps) => {
   return (
-    <div className="todo__field field">
+    <div className={clsx('field', className)}>
       <label
         className="field__label"
-        htmlFor="new-task"
+        htmlFor={id}
       >
-        New task
+        {label}
       </label>
       <input
         className="field__input"
-        id="new-task"
+        id={id}
         placeholder=" "
         autoComplete="off"
+        {...props}
       />
     </div>
   )
